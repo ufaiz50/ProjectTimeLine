b7038da53156ffe172eed364a8bc4c9dd44775c1
umar faiz <ufaiz50@gmail.com>
2021-07-13 13:38:26 +0700

    Add Client

---
+++ Client/BaseController/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.BaseController
+{
+    public class Address
+    {
+    }
+}
